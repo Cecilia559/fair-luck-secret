a38a8f3c00acfb0320d004f8964317bdbd1e70d5
Cecilia559 <MirandaWinifredqrigs@outlook.com>
2025-11-07 09:27:08 +0800

    fix: Update Sepolia contract address in addresses config

--- ui/abi/FHERaffleAddresses.ts
+++ ui/abi/FHERaffleAddresses.ts
@@ -3,7 +3,7 @@
   Command: 'npm run genabi'
 */
 export const FHERaffleAddresses = {
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0xFfFC34216aCa5Ed8597AE3B1EEd884C19cfbD9FB", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
 };
 
